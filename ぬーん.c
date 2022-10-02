33ebe8b9c640535b585598f86d36d33b1e29f023
ゆききつね <107163439+Koyou-nana@users.noreply.github.com>
2022-10-02 13:34:44 +0900

    分かりやすく
    2回目コミット

--- ぬーん.c
+++ ぬーん.c
@@ -1,35 +1,34 @@
-﻿#include <stdio.h> // scanf関数やprintf関数を使うため
-#include <stdlib.h> //exit関数を使うため
+﻿#include <stdio.h>	// scanf関数やprintf関数を使うため
+#include <stdlib.h>	// exit関数を使うため
 void main()
 {
-	char o;
-	int x, y, s;
+	char o;	// 文字型(チャー,キャラ)の変数oを宣言
+	int x, y, s;	// 整数型(イント)のx,y,sを宣言
 	printf("ope?");
-	scanf("%c", &o);	// キーボードからoに演算子を入力する
+	scanf("%c", &o);	// printf関数で質問し、その答えをscanf関数でそれぞれの変数に代入する。7～12行目まで同じ。
 	printf("x=?");
-	scanf("%d", &x);	// キーボードからxに整数を入力する
+	scanf("%d", &x);
 	printf("y=?");
-	scanf("%d", &y);	// キーボードからyに整数を入力する
+	scanf("%d", &y);
 
-	if (o == '+') {        // 演算子が+の場合
+	if (o == '+') {		// if文を使って指定された演算子を確認し、それに応じた計算をし、変数sに代入する。。14～28行目まで
 		s = x + y;
 	}
-	else if (o == '-') {   // 演算子が-の場合
+	else if (o == '-') {
 		s = x - y;
 	}
-	else if (o == '*') {   // 演算子が*の場合
+	else if (o == '*') {
 		s = x * y;
 	}
-	else if (o == '/') {   // 演算子が/の場合
+	else if (o == '/') {
 		s = x / y;
 	}
-	else if (o == '%') {   // 演算子が上記以外の場合
+	else if (o == '%') {	// %は割り算した余りを出す。
 		s = x % y;
 	}
-	else {
+	else {		// 演算子が間違っている場合の処理。演算子が間違っています。と出力し終了する。
 		printf("演算子が間違っています。");
 		exit(0);
 	}
-
-	printf("%d %c %d = %d", x, o, y, s);
+	printf("%d %c %d = %d", x, o, y, s);	//計算結果を出力する。
 }
