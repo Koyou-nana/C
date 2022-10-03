d9fe77427550e7f666b694ad2388a54b604596d5
ゆききつね <107163439+Koyou-nana@users.noreply.github.com>
2022-10-03 19:33:37 +0900

    自作したもの
    1st commit

---
+++ 数当て/数当て.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+	int a, i;
+	printf("4桁以内で数字を決めてください。\n");
+	scanf("%d", &a);
+	if (a > 10000) {
+		printf("範囲を超えています。\n");
+		exit(1);
+	}
+	for (i = 0; i < 10000; i++) {
+		if (a == i) {
+			printf("%04d = %04d true\n数値が一致しました。\nあなたのパスワードは%04dです。", a, i, i);
+			exit(0);
+		}
+		else {
+			printf("%04d = %04d false\n", a, i);
+		}
+	}
+}
