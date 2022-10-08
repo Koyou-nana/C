0b527445dd5a40c3051f07e22f50c5a58f992ade
ゆききつね <107163439+Koyou-nana@users.noreply.github.com>
2022-10-08 16:03:52 +0900

    バグの演奏プログラム
    1st commit

---
+++ バグ/バグ.cpp
@@ -0,0 +1,152 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+// 音階を簡単に
+#define A35 233
+#define B3 246
+#define C4 261
+#define C45 277
+#define D4 293
+#define D45 311
+#define E4 329
+#define F4 349
+#define F45 469
+#define G4 391
+#define G45 415
+#define A4 440
+#define A45 466
+#define B4 493
+#define C5 523
+#define C55 544
+#define D5 587
+#define D55 622
+#define E5 659
+#define F5 698
+#define F55 739
+
+void main()
+{
+	printf("絶叫な感情落下　パッパラノーイ「ア」\n");
+	Beep(C45, 600);
+	Beep(A45, 300);
+	Beep(C45, 300);
+	Beep(F4, 300);
+	Beep(C5, 300);
+	Beep(A45, 300);
+	Beep(A4, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(C5, 300);
+	Beep(A45, 300);
+	Beep(F5, 985);
+	printf("溺れ声上げては　ぐるぐる\n");
+	Beep(F5, 300);
+	Beep(E5, 300);
+	Beep(G4, 600);
+	Beep(G45, 300);
+	Beep(A4, 300);
+	Beep(F5, 600);
+	Beep(C5, 300);
+	Beep(C55, 300);
+	Beep(F4, 300);
+	Beep(F4, 300);
+	Beep(F4, 700);
+	Sleep(300);
+	printf("まあ！絶体絶命落下　やったラ滅多ラ\n");
+	Beep(G45, 600);
+	Beep(C45, 600);
+	Beep(A45, 300);
+	Beep(C45, 300);
+	Beep(F4, 300);
+	Beep(C5, 300);
+	Beep(A45, 300);
+	Beep(A4, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(C5, 300);
+	Beep(A45, 300);
+	Beep(F5, 985);
+	printf("沈めユメユメ\n");
+	Beep(A45, 300);
+	Beep(F5, 300);
+	Beep(F55, 300);
+	Beep(F5, 300);
+	Beep(F55, 300);
+	Beep(F5, 300);
+	Beep(A45, 300);
+	printf("嫌嫌嫌\n");
+	Sleep(300);
+	Beep(C55, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(A45, 600);
+	printf("さあ バ　バ　バグさ　バグバグ\n");
+	Beep(G45, 600);
+	Beep(A45, 300);
+	Sleep(300);
+	Beep(A45, 300);
+	Sleep(300);
+	Beep(A45, 300);
+	Beep(F4, 300);
+	Sleep(300);
+	Beep(A45, 450);
+	Beep(A45, 300);
+	Beep(F45, 300);
+	Beep(A45, 300);
+	Beep(F4, 300);
+	Sleep(300);
+	printf("的ハズレズレ　慈愛　嫌嫌\n");
+	Beep(A45, 300);
+	Beep(F5, 300);
+	Beep(F55, 300);
+	Beep(F5, 300);
+	Beep(F55, 300);
+	Beep(F5, 300);
+	Beep(A45, 300);
+	Sleep(300);
+	Beep(C55, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(A45, 600);
+	printf("さあ バ　バ　バグさ　バグバグ\n");
+	Beep(G45, 600);
+	Beep(C55, 300);
+	Sleep(300);
+	Beep(C55, 300);
+	Sleep(300);
+	Beep(C55, 300);
+	Beep(F45, 300);
+	Sleep(300);
+	Beep(C55, 450);
+	Beep(C55, 300);
+	Beep(C55, 300);
+	Beep(C55, 300);
+	Beep(A45, 600);
+	printf("解答絶え絶え　嫌嫌嫌　嫌嫌嫌\n");
+	Beep(A45, 300);
+	Beep(F5, 300);
+	Beep(F55, 300);
+	Beep(F5, 300);
+	Beep(F55, 300);
+	Beep(F5, 300);
+	Beep(A45, 300);
+	Sleep(300);
+	Beep(C55, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(A45, 300);
+	Beep(C55, 300);
+	Beep(A45, 600);
+	Sleep(300);
+	Beep(F5, 300);
+	Beep(C55, 300);
+	Beep(F5, 300);
+	Beep(C55, 300);
+	Beep(F5, 300);
+	Beep(C55, 600);
+	printf("バグ／かいりきベア\n\n");
+	printf("プログラム作成->小葉　七夜\n");
+}
