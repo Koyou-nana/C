4d617926f91720086a0c4f55b443e40046c74755
ゆききつね <107163439+Koyou-nana@users.noreply.github.com>
2023-03-01 15:44:05 +0900

    Update 文字列あて.c

--- 文字列当てゲーム/文字列あて.c
+++ 文字列当てゲーム/文字列あて.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-void makestr(int num);  // 文字列作成
+char* makestr(int num, char* str);  // 文字列作成
 // numはmadestr関数の引数
-int strcheck(char* str2);// 文字列確認(文字)
+int strcheck(char* str2, char* str, int len);// 文字列確認(文字)
 //str2はstrcheckの引数
-int lencheck(char* lenstr);// 文字列確認(長さ)
+int lencheck(char* lenstr, int len, int len2);// 文字列確認(長さ)
 //lenstrはlencheckの引数
-char str[1000];     //作成された文字列用配列
-char Anser[1000]; //ユーザーの決めた文字列用配列
-int len, len2;  // lenは作成された文字列の長さ。len2は作成された文字列の長さ-ユーザーが決めた文字列の長さ
 
 void main()
 {
+    char str[1000];     //作成された文字列用配列
+    char Anser[1000]; //ユーザーの決めた文字列用配列
+    int len, len2;  // lenは作成された文字列の長さ。len2は作成された文字列の長さ-ユーザーが決めた文字列の長さ
     char Enter[700];
     int n;
     printf("文字列あてゲーム\n");
@@ -22,7 +22,8 @@ void main()
     gets(Enter);
     srand(time(NULL));
     n = rand() % 10 + 1;
-    makestr(n);
+    makestr(n, str);
+    len = strlen(str);
     printf("文字列が作成されました。ゲームを開始しましょう。Enterキーを押してください。\n");
     gets(Enter);
     printf("ゲーム開始、作成された単語を推測し入力してください。\n");
@@ -30,9 +31,9 @@ void main()
     {
         scanf("%s", Anser);
         printf("%sですね!さて結果は...\n", Anser);
-        printf("文字列の長さの違い(余り)...%d\n", lencheck(Anser));
+        printf("文字列の長さの違い(余り)...%d\n", lencheck(Anser, len, len2));
         printf("文字列の適合...\n");
-        n = strcheck(Anser);
+        n = strcheck(Anser, str, len);
         if (n == 1){
             printf("おめでとうございます！見事隠された文字列%sを見つけられました!\n", str);
             exit(0);
@@ -44,42 +45,34 @@ void main()
 
 }
 
-void makestr(int num)
+char* makestr(int num, char* str)
 {
     if (num == 1){
         strcpy(str, "bike");
-        len = 4;
     } else if (num == 2){
         strcpy(str, "left");
-        len = 4;
     } else if (num == 3){
         strcpy(str, "right");
-        len = 5;
     } else if (num == 4){
         strcpy(str, "win");
-        len = 3;
     } else if (num == 5){
         strcpy(str, "computer");
-        len = 8;
     } else if (num == 6){
         strcpy(str, "list");
-        len = 4;
     } else if (num == 7){
-        strcpy(str, "japan");
-        len = 5;
+        strcpy(str, "game");
     } else if (num == 8){
         strcpy(str, "shrine");
-        len = 6;
     } else if (num == 9){
         strcpy(str, "copy");
-        len = 4;
     } else if (num == 10){
         strcpy(str, "number");
-        len = 6;
     }
+
+    return(str);
 }
 
-int lencheck(char* lenstr)
+int lencheck(char* lenstr, int len, int len2)
 {
     int checklen;
     checklen = strlen(lenstr);
@@ -88,13 +81,16 @@ int lencheck(char* lenstr)
     return len2;
 }
 
-int strcheck(char* str2)
+int strcheck(char* str2, char* str, int len)
+// str2=Anser(ユーザーの回答) str=str(正解) len=作成された文字列の長さ
 {
     char* p = str;
+    // char型ポインタに配列strのアドレスを格納
     int i, j, m;
     int n = 0;
     char b;
     m = strlen(str2);
+    // 作成された文字列の長さをmに代入
     for (i = 1; i <= m; i++){
         j = i - 1;
         b = str2[j];
